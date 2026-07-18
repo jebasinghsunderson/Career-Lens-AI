007b4dd4c1ffb2a003a0098b79fa9825d8fc1812
edlynjessica <edlynjessica09@gmail.com>
2026-07-18 13:41:43 +0530

    Added test preprocessing utilites

--- backend/test_for_pdfExtraction.py
+++ backend/test_for_pdfExtraction.py
@@ -1,5 +1,8 @@
 from resume_parser.pdf_extractor import extract_text
+from resume_parser.utils import clean_text
 
 text = extract_text("sample_resumes/Edlyn Resume.pdf")
 
-print(text)
+cleaned = clean_text(text)
+
+print(cleaned)
